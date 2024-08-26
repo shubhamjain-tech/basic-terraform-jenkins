c6ae53b9256687ecfcfdf71f6f73da688a486187
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-08-26 14:31:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAYS2NXLMFQHCZ2ENF"
-  secret_key = "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"
+  access_key = "AKIAYS2NXLMFVURV75W6"
+  secret_key = "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"
