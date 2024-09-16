19a31dec548bad2becd1411f820ff354c2c29221
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-09-16 14:15:10 +0530

    s3 bucket resource added

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "jenkins-with-terraform-2024-09-16"
+  tags = {
+    Name        = "MY bucket"
+    Environment = "Dev"
+  }
+}
